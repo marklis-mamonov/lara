aecf89080c3cf575612393891fa260ab57a4b6a8
marklis-mamonov <marklis-mamonov@yandex.ru>
2019-07-25 18:51:55 +0300

    Корректировка строк

--- database/migrations/2019_07_25_104232_create_blog_posts_table.php
+++ database/migrations/2019_07_25_104232_create_blog_posts_table.php
@@ -16,8 +16,8 @@ class CreateBlogPostsTable extends Migration
         Schema::create('blog_posts', function (Blueprint $table) {
             $table->bigIncrements('id');
 
-            $table->string('category_id')->unsigned();
-            $table->string('user_id')->unsigned();
+            $table->bigInteger('category_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
 
             $table->string('slug')->unique();
             $table->string('title');
