a2d07ec9abe47cad466cfcaa6f93a66c812f08c9
marklis-mamonov <marklis-mamonov@yandex.ru>
2019-07-30 11:47:52 +0300

    Валидация

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Blog\Admin;
 
+use App\Http\Requests\BlogCategoryUpdateRequest;
 use App\Models\BlogCategory;
 use Illuminate\Http\Request;
 
@@ -61,8 +62,9 @@ class CategoryController extends BaseController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(BlogCategoryUpdateRequest $request, $id)
     {
+
         $item = BlogCategory::find($id);
         if (empty($item)) {
             return back()
