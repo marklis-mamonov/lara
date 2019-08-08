d4e5a1eedba18ea8720251822046d4a4ff3c708b
marklis-mamonov <marklis-mamonov@yandex.ru>
2019-08-08 18:48:26 +0300

    Связь с родительской категорией

--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -8,11 +8,46 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class BlogCategory extends Model
 {
   use SoftDeletes;
-
+  
+  const ROOT = 1;
+  
   protected $fillable = [
     'title',
     'slug',
     'parent_id',
     'description'
   ];
+
+  /**
+   * Получить родительскую категорию
+     * @return BlogCategory
+     */
+    public function parentCategory()
+    {
+        return $this->belongsTo(BlogCategory::class, 'parent_id', 'id');
+    }
+
+    /**
+     * Пример аксессуара
+     * 
+     * @return string
+     */
+    public function getParentTitleAttribute()
+    {
+      $title = $this->parentCategory->title
+        ?? ($this->isRoot()
+        ? 'Корень'
+        : '???');
+        return $title;
+    }
+
+    /**
+     * Является ли текущий объект корневым
+     * 
+     * @return bool
+     */
+    public function isRoot()
+    {
+        return $this->id === BlogCategory::ROOT;
+    }
 }
